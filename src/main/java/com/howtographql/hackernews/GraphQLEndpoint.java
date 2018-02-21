ef5737c41aed711e4d5330685ef384225e9cd49c
Wilkinsleung <wilkinsweiqiangliang@gmail.com>
2018-02-21 23:08:20 +1100

    adding simple mutation for link

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -8,19 +8,19 @@ import graphql.servlet.SimpleGraphQLServlet;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/graphql")
-public class GraphQLEndpoint extends SimpleGraphQLServlet{
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
     public GraphQLEndpoint() {
         super(buildSchema());
     }
 
-    private static GraphQLSchema buildSchema(){
+    private static GraphQLSchema buildSchema() {
 
         LinkRepository linkRepository = new LinkRepository();
 
         return SchemaParser.newParser()
                 .file("schema.graphqls") //parse the schema file created earlier
-                .resolvers(new Query(linkRepository))
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
                 .build()
                 .makeExecutableSchema();
     }
